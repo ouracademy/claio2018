0f0b71d7c3efbffe68dd26b530debdb75419d323
Diana Quintanilla <qpdiam@gmail.com>
2017-08-23 22:15:15 -0500

    chore: v0.0.1

---
+++ _next/b9f761c4-0f1f-4a37-98d8-b85e1d532f34/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([2],[],[715]);
+            return { page: comp.default }
+          })
+        
